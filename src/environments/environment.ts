984d15fb2ccc4fdd7128d7e640939a0314dabbd3
ianlabicani <labicaniian@gmail.com>
2024-02-07 18:24:25 +0800

    follow convention from morony

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    projectId: 'capstone-63f36',
+    projectId: 'demo-project',
     appId: '1:969073261181:web:719ff686dc905fe4afcf14',
     storageBucket: 'capstone-63f36.appspot.com',
     apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
