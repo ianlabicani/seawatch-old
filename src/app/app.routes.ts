824c891d861fd0b4a4de5f84f1a63c6e50a8cecb
ianlabicani <labicaniian@gmail.com>
2024-02-07 17:18:28 +0800

    make redirectTo to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,19 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+  },
+  {
     path: 'home',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
 ];
