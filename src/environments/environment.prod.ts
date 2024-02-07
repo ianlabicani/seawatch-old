afc1decd1706de095df7090127c5feeb1d99c39d
ianlabicani <labicaniian@gmail.com>
2024-02-07 16:22:17 +0800

    add firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    projectId: 'capstone-63f36',
+    appId: '1:969073261181:web:719ff686dc905fe4afcf14',
+    storageBucket: 'capstone-63f36.appspot.com',
+    apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
+    authDomain: 'capstone-63f36.firebaseapp.com',
+    messagingSenderId: '969073261181',
+  },
+  useEmulators: false,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    projectId: 'capstone-63f36',
+    appId: '1:969073261181:web:719ff686dc905fe4afcf14',
+    storageBucket: 'capstone-63f36.appspot.com',
+    apiKey: 'AIzaSyDqN-da0lcsO7G90djx0rTrqCihuQOrheg',
+    authDomain: 'capstone-63f36.firebaseapp.com',
+    messagingSenderId: '969073261181',
+  },
+  useEmulators: true,
 };
 
 /*
