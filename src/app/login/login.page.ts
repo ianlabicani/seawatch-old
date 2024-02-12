d1a7b98e9c1a500a0dc3852946fbe0c2dd2a0b21
ianlabicani <labicaniian@gmail.com>
2024-02-12 21:13:31 +0800

    Refactor login page component and add form validation

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
-  FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router, RouterModule } from '@angular/router';
+
+import { addIcons } from 'ionicons';
+import { informationCircleOutline } from 'ionicons/icons';
+import { FirebaseError } from '@angular/fire/app';
 
 @Component({
   selector: 'app-login',
@@ -22,12 +27,31 @@ export class LoginPage {
   afAuth = inject(AngularFireAuth);
   router = inject(Router);
   form: FormGroup;
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'email', message: 'Email must be a valid email address.' },
+    ],
+    password: [
+      { type: 'required', message: 'Password is required.' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 6 characters long.',
+      },
+    ],
+  };
+  submitError = '';
 
   constructor() {
     this.form = this.fb.group({
-      email: [],
-      password: [],
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
     });
+
+    addIcons({ informationCircleOutline });
   }
 
   onSubmit() {
@@ -43,6 +67,7 @@ export class LoginPage {
         this.router.navigate(['/home']);
       });
     } catch (error) {
+      this.submitError = (error as unknown as FirebaseError).message;
       console.log(error);
     }
   }
