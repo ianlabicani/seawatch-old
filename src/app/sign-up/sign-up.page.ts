950834e48232619f8f5825056b74de31b2549445
ianlabicani <iablabicani@gmail.com>
2024-02-13 08:53:20 +0800

    Refactor sign-up page component and form validation - need fix

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
-  FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router, RouterLink } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+import { addIcons } from 'ionicons';
+import { informationCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-sign-up',
@@ -22,27 +26,48 @@ export class SignUpPage {
   afAuth = inject(AngularFireAuth);
   router = inject(Router);
   form: FormGroup;
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'email', message: 'Email must be a valid email address.' },
+    ],
+    password: [
+      { type: 'required', message: 'Password is required.' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 6 characters long.',
+      },
+    ],
+    confirmPassword: [
+      { type: 'required', message: 'Confirm Password is required.' },
+      { type: 'mismatch', message: "Password doesn't match." },
+    ],
+  };
+  submitError = '';
 
   constructor() {
-    this.form = this.fb.group({
-      email: [],
-      password: [],
-      confirmPassword: [],
-    });
+    this.form = this.fb.group(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      },
+      { validators: this.passwordMatchValidator }
+    );
+    addIcons({ informationCircleOutline });
   }
 
-  get isPasswordMatch() {
-    return (
-      this.form.get('password')?.value ==
-      this.form.get('confirmPassword')?.value
-    );
+  passwordMatchValidator(formGroup: FormGroup) {
+    return formGroup.get('password')?.value ===
+      formGroup.get('confirmPassword')?.value
+      ? null
+      : { mismatch: true };
   }
 
   onSubmit() {
-    if (!this.isPasswordMatch) {
-      console.log("password don't match");
-      return;
-    }
     const email = this.form.get('email')?.value;
     const password = this.form.get('password')?.value;
     this.signUp(email, password);
